52b478d3dd96346805a428ae0adde768f55627da
Liyang Wang <wangliyang@google.com>
2023-10-23 08:44:29 +0800

    add the initial features: db, auth, etc.

---
+++ app.py
@@ -0,0 +1,125 @@
+from tornado import web, ioloop, escape, auth
+from motor import motor_tornado
+
+import os
+
+from generated.proto import wish_item_pb2
+from storage_layer import StorageLayer
+
+class BaseHandler(web.RequestHandler):
+    def get_current_user(self):
+        return self.get_signed_cookie("user_public_id")
+
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        self.write("<p>Please login</p><ul>")
+        self.write('<li><a href="auth/google">Login with Google</a></li></ul>')
+
+
+class GoogleOAuth2LoginHandler(web.RequestHandler,
+                               auth.GoogleOAuth2Mixin):
+    def initialize(self):
+        self._storage = StorageLayer(self.settings["db_client"])
+
+    async def get(self):
+        if self.get_argument('code', False):
+            access = await self.get_authenticated_user(
+                redirect_uri='http://localhost:5000/auth/google',
+                code=self.get_argument('code'))
+            user = await self.oauth2_request(
+                "https://www.googleapis.com/oauth2/v1/userinfo",
+                access_token=access["access_token"])
+
+            # store into db if it's a new user
+            email = escape.xhtml_escape(user["email"])
+            name = escape.xhtml_escape(user["name"])
+            self.set_signed_cookie("user_public_id", await self._storage.get_user_public_id(email, name))
+            self.redirect("/")
+
+        else:
+            self.authorize_redirect(
+                redirect_uri='http://localhost:5000/auth/google',
+                client_id=self.get_google_oauth_settings()['key'],
+                scope=['profile', 'email'],
+                response_type='code',
+                extra_params={'approval_prompt': 'auto'})
+
+
+class IndexHandler(BaseHandler):
+    def initialize(self):
+        self._storage = StorageLayer(self.settings["db_client"])
+
+    @web.authenticated
+    async def get(self):
+        _logged_in_account = await self._storage.get_user_profile(escape.xhtml_escape(self.current_user))
+        self.write('Hello %s! <a href="%s">Here</a> is your wishlist.' %
+            (_logged_in_account.name, _logged_in_account.public_id))
+
+class WishlistHandler(web.RequestHandler):
+    def initialize(self):
+        self._storage = StorageLayer(self.settings["db_client"])
+
+    def populate_wish_item(self, id, name, link_url, shop_type):
+        wish_item = wish_item_pb2.WishItem()
+        wish_item.id = id
+        wish_item.name = name
+        link = wish_item.shopping_links.add()
+        link.url = link_url
+        link.shop_type = shop_type
+        return wish_item
+
+    async def get(self, public_id):
+        account = await self._storage.get_user_profile(public_id)
+
+        wish_items = []
+        wish_items.append(
+            self.populate_wish_item(1,
+                                    "Leica M11",
+                                    "https://leica-store.sg/products/leica-m11-silver",
+                                    wish_item_pb2.WishItemShoppingLink.ShopType.SHOP_TYPE_UNSPECIFIED))
+        wish_items.append(
+            self.populate_wish_item(2,
+                                    "Leica Elmarit-M 28mm F/2.8 ASPH",
+                                    "https://leica-store.sg/products/leica-elmarit-m-28mm-f-2-8-asph-black",
+                                    wish_item_pb2.WishItemShoppingLink.ShopType.SHOP_TYPE_UNSPECIFIED))
+        wish_items.append(
+            self.populate_wish_item(3,
+                                    "PlayStation 5",
+                                    "https://www.amazon.com/PlayStation-5-Console-CFI-1215A01X/dp/B0BCNKKZ91/ref=sr_1_3?crid=DP4ND9EB4I9J&keywords=ps5&qid=1697793757&sprefix=ps%2Caps%2C312&sr=8-3&th=1",
+                                    wish_item_pb2.WishItemShoppingLink.ShopType.SHOP_TYPE_AMAZON))
+        self.render("wishlist.html",
+                    account=account,
+                    wish_items=wish_items)
+
+
+def main():
+    settings = {
+		"template_path": os.path.join(os.path.dirname(__file__), "templates"),
+		"static_path": os.path.join(os.path.dirname(__file__), "static"),
+        "cookie_secret": "aafDFWcAEefq323gq34rwKadc",
+        "login_url": "/login",
+        "google_oauth": {"key": "572658144253-2ifc8o8vj8ufml8kt6k2caddi5d4othp.apps.googleusercontent.com", "secret": "VQULx6mUU95euAAXmxoIf9jd"},
+        "db_client": motor_tornado.MotorClient("mongodb+srv://wangliyangleon:jdsFsTpNPwZkSKxh@cluster0.skjbcmx.mongodb.net/?retryWrites=true&w=majority"),
+		"debug" : True
+	}
+
+    app = web.Application(
+		[
+			(r'/', IndexHandler),
+            (r'/login', LoginHandler),
+            (r'/auth/google', GoogleOAuth2LoginHandler),
+			(r'/([a-zA-Z0-9]+)', WishlistHandler),
+			# (r'/ws', WebSocketHandler),
+			# (r'/api', ApiHandler),
+		], **settings
+	)
+
+
+    port = int(os.environ.get("PORT", 5000))
+    print('Listening on port: %s' % port)
+    app.listen(port)
+    ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
---
+++ storage_layer.py
@@ -0,0 +1,56 @@
+import random
+import string
+
+from generated.proto import account_pb2
+
+class StorageLayer():
+    def __init__(self, db_client):
+        self._db = db_client.tgotm
+
+    async def create_new_user(self, email: str, name: str) -> str:
+        chars = string.ascii_uppercase + string.ascii_lowercase +  string.digits
+        public_id = ""
+        while True:
+            public_id = ''.join(random.choice(chars) for _ in range(8))
+            print("Try creating new user %s for: %s" % (public_id, email))
+            if await self._db["accounts"].find_one({"public_id": public_id}) is None:
+                break
+
+        print("Creating new user %s for: %s" % (public_id, email))
+        new_account = {
+            "public_id": public_id,
+            "email": email,
+            "name": name,
+            "avatar_url" : "placeholder.images.com",
+        }
+        await self._db["accounts"].insert_one(new_account)
+        return public_id
+
+    async def get_existing_public_id(self, email: str) -> str:
+        if (account := await self._db["accounts"].find_one({"email": email})) is not None:
+            return account["public_id"]
+        else:
+            return ""
+
+    async def get_user_public_id(self, email: str, name: str) -> str:
+        public_id = await self.get_existing_public_id(email)
+        if not public_id:
+            public_id = await self.create_new_user(email, name)
+        return public_id
+
+    async def get_user_profile(self, public_id: str) -> account_pb2.Account:
+        print("Getting profile for account %s" % public_id)
+        account = account_pb2.Account()
+        if (existing_account := await self._db["accounts"].find_one({"public_id": public_id})) is not None:
+            account.public_id = existing_account["public_id"]
+            account.email = existing_account["email"]
+            account.name = existing_account["name"]
+            account.avatar_url = existing_account["avatar_url"]
+        else:
+            print("Public id %s not found" % public_id)
+            account.public_id = public_id
+            account.email = "N/A"
+            account.name = "N/A"
+            account.avatar_url = "N/A"
+        return account
+
